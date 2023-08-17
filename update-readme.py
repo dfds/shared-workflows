ac6a9b62e2a9a76b16b45df44acfbf13b601190d
Mikkel Nordskjold <miknord@dfds.com>
2023-08-17 13:41:53 +0200

    Updated README python script

--- update-readme.py
+++ update-readme.py
@@ -2,6 +2,7 @@
 
 import os
 import yaml
+import pathlib
 
 # Line break
 def lb():
@@ -22,7 +23,6 @@ def rootReadmeHeader():
 
     return headlineContent
 
-repoUri = 'https://github.com/dfds/shared-workflows'
 examplesDir = 'examples'
 headerReadmeContent = rootReadmeHeader()
 bodyReadmeConetent = []
@@ -31,6 +31,9 @@ collection = {}
 # Create example collection
 for example in sorted(os.listdir(examplesDir)):
     examplePath = os.path.join(examplesDir, example)
+    name = pathlib.Path(examplePath).stem
+    workflowPath = os.path.join('.github/workflows', example)
+    actionPath = os.path.join('.github/actions', name, 'action.yml')
 
     # Ignore folders here
     if os.path.isdir(examplePath):
@@ -39,63 +42,87 @@ for example in sorted(os.listdir(examplesDir)):
     # Extract the category from filename
     category = example.split('-')[0]
 
+    # Check if accompanied file exist
+    if os.path.isfile(workflowPath):
+        sharedType = 'workflows'
+    elif os.path.isfile(actionPath):
+        sharedType = 'actions'
+    else:
+        continue
+
     if not category in collection.keys():
-        collection[category] = []
+        collection[category] = {}
+
+    if not sharedType in collection[category].keys():
+        collection[category][sharedType] = []
     
-    collection[category].append(examplePath)
+    collection[category][sharedType].append(examplePath)
 
 for category in collection:
-    # Insert the category list item in the index README
-    headerReadmeContent += lb() + '- [' + category.title() + '](' + repoUri + '#' + category + ')'
+    # Insert the category list item in the README index
+    headerReadmeContent += lb() + '- [' + category.title() + '](#' + category + ')'
 
     # Initialize the category section
     currentCategory = dlb() + '## ' + category.title()
     
     # Loop through every example in the category
-    for examplePath in collection[category]:
-        # Here we have to read the example yaml
-        with open(examplePath, 'r') as stream:
-            yml = yaml.safe_load(stream)
+    for sharedType in collection[category]:
+        # Insert the shared type item in the index README
+        headerReadmeContent += \
+            lb() + ind() + \
+            '- ' + sharedType
+        
+        for examplePath in collection[category][sharedType]:
+            # Here we have to read the example yaml
+            with open(examplePath, 'r') as stream:
+                yml = yaml.safe_load(stream)
 
-            if 'name' not in yml:
-                continue
-            
-            # Insert the example list item in the index README
-            nameUriEncoded = yml['name'].replace(' ', '-').lower()
-            headerReadmeContent += \
-                lb() + ind() + \
-                '- [' + yml['name'] + '](' + repoUri + '#' + nameUriEncoded + ')'
-            
-            # Example title
-            currentCategory += dlb() + '### ' + yml['name']
+                if 'name' not in yml:
+                    continue
+                
+                # Convert current type from plural to singular
+                currentType = sharedType[:-1]
+                
+                # Insert the example list item in the index README
+                nameUriEncoded = yml['name'].replace(' ', '-').lower()
+                headerReadmeContent += \
+                    lb() + ind() + ind() + \
+                    '- [' + yml['name'] + '](#' + nameUriEncoded + ')'
+                
+                # Example title
+                currentCategory += dlb() + '### ' + yml['name']
+                
+                # Example type
+                grammar = 'a' if currentType == 'workflow' else 'an'
+                currentCategory += dlb() + '_This is ' + grammar + ' ' + currentType + '_'
 
-            # Example description
-            if 'description' in yml:
-                currentCategory += dlb() + yml['description']
-                del yml['description']
+                # Example description
+                if 'description' in yml:
+                    currentCategory += dlb() + yml['description']
+                    del yml['description']
 
-            # Example author
-            if 'author' in yml:
-                currentCategory += dlb() + '[Marketplace](' + yml['author'] + ')'
-                del yml['author']
-            
-            # Example code
-            currentCategory += dlb() + 'How to invoke this shared workflow:'
-            
-            import re
-            fc = open(examplePath, 'r')
-            # Remove description from example
-            content = re.sub(r'^description.*\n?', '', fc.read(), flags=re.MULTILINE)
-            # Remove author from example
-            content = re.sub(r'^author.*\n?', '', content, flags=re.MULTILINE)
-            fc.close()
+                # Example author
+                if 'author' in yml:
+                    currentCategory += dlb() + '[Marketplace](' + yml['author'] + ')'
+                    del yml['author']
+                
+                # Example code
+                currentCategory += dlb() + 'How to invoke this ' + currentType + ':'
+                
+                import re
+                fc = open(examplePath, 'r')
+                # Remove description from example
+                content = re.sub(r'^description.*\n?', '', fc.read(), flags=re.MULTILINE)
+                # Remove author from example
+                content = re.sub(r'^author.*\n?', '', content, flags=re.MULTILINE)
+                fc.close()
 
-            currentCategory += dlb() + \
-                '```yaml' + \
-                lb() + \
-                content + \
-                lb() + \
-                '```'
+                currentCategory += dlb() + \
+                    '```yaml' + \
+                    lb() + \
+                    content + \
+                    lb() + \
+                    '```'
     
     # Write it to body README
     bodyReadmeConetent.append(currentCategory)
